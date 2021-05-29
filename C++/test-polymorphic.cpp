c900621551aa66b0d2ae04ff793461a1226e3a8f
Joris P <71002181+Miisterone@users.noreply.github.com>
2021-05-29 16:20:38 +0200

    Update files

---
+++ C++/test-polymorphic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A{
+public:
+    virtual void show(){
+        cout << "Je suis A" << endl;
+    }
+
+};
+
+class B: public A{
+public:
+    virtual void show(){
+        cout << "Je suis B" << endl;
+    }
+};
+
+
+void affiche(A& a){
+    a.show();
+}
+
+int main(){
+
+    A a;
+    B b;
+
+    affiche(a);
+    affiche(b);
+
+    return 0;
+}
